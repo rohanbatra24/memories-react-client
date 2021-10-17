c9281a6704df8c2b1ad4642bf14b64b1767d0efd
Rohan Batra <rohanbatra24@gmail.com>
2021-10-16 22:37:08 -0400

    fix bug - return from createPost

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const url = "http://localhost:5000/posts";
 export const fetchPosts = () => axios.get(url);
 
 export const createPost = (newPost) => {
-  axios.post(url, newPost);
+  return axios.post(url, newPost);
 };
 
 export const updatePost = (id, updatedPost) => {
