0fe2e1a52240d34133e89c1c03fe8305159b2228
Rohan Batra <rohanbatra24@gmail.com>
2021-10-16 23:38:17 -0400

    add action constants for redux

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,20 +1,28 @@
+import {
+  CREATE,
+  DELETE,
+  FETCH_ALL,
+  LIKE,
+  UPDATE,
+} from "../constants/actionTypes";
+
 const reducer = (posts = [], action) => {
   switch (action.type) {
-    case "FETCH ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
-    case "UPDATE": {
+    case UPDATE: {
       return posts.map((post) => {
         return post._id === action.payload._id ? action.payload : post;
       });
     }
-    case "DELETE": {
+    case DELETE: {
       return posts.filter((post) => {
         return post._id !== action.payload;
       });
     }
-    case "LIKE":
+    case LIKE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
