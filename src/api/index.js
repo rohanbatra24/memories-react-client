ae36f0e6dd5072f2ad47f3e84172e60e869ab29a
Rohan Batra <rohanbatra24@gmail.com>
2021-11-14 11:35:49 -0500

    update backend API URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3001" });
+const API = axios.create({
+  baseURL: "https://memories-project907.herokuapp.com",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
